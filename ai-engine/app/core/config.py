2ebab91d4b3bb7a672498461cf6b21deb4fdc425
clay04 <clayaikenmangeberjr@gmail.com>
2026-05-22 17:05:41 +0800

    Change groq model

--- ai-engine/app/core/config.py
+++ ai-engine/app/core/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = int(os.getenv("REDIS_PORT"))
 
 MONGODB_URI = os.getenv("MONGODB_URI")
--- ai-engine/app/services/llm/groq_service.py
+++ ai-engine/app/services/llm/groq_service.py
@@ -4,6 +4,6 @@ from app.core.config import GROQ_API_KEY
 
 groq_llm = ChatGroq(
     groq_api_key=GROQ_API_KEY,
-    model_name="llama3-70b-8192",
+    model_name="llama-3.3-70b-versatile",
     temperature=0.1
 )
