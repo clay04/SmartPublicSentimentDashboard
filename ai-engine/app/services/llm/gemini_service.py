49ea68495ef18d7c8d07dd82588b3b872dc39886
clay04 <clayaikenmangeberjr@gmail.com>
2026-05-22 17:31:45 +0800

    fix llm service,

--- ai-engine/app/services/llm/gemini_service.py
+++ ai-engine/app/services/llm/gemini_service.py
@@ -5,6 +5,6 @@ from app.core.config import GEMINI_API_KEY
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     google_api_key=GEMINI_API_KEY,
-    temperature=0.2
+    temperature=0.2,
     max_retries=1
 )
