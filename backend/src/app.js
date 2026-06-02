0b7a69775c94699e59534d3ed1994c89045c05fa
clay04 <clayaikenmangeberjr@gmail.com>
2026-06-02 22:50:27 +0800

    Logger Backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(`📥 ${req.method} ${req.url}`);
+    next();
+});
+
 app.use("/ai", aiRoutes);
 app.use("/chat", chatRoutes);
 
--- backend/src/controllers/ai.controller.js
+++ backend/src/controllers/ai.controller.js
@@ -2,16 +2,22 @@ import { SaveAIREsult } from "../services/ai.service.js";
 
 export const ingestAI = async (req, res) => {
     try {
+        console.log("🔥 AI INGEST HIT");
+        console.log("📦 BODY:", req.body);
+
         const data = req.body;
 
         const saved = await SaveAIREsult(data);
+
+        console.log("✅ SAVED TO DB:", saved._id);
 
         res.json({
             message: "AI Result Save",
             data: saved
         });
+
     } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Failed" })
+        console.error("❌ ERROR:", err);
+        res.status(500).json({ error: "Failed" });
     }
-}
+};
