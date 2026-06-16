25c076d1b1dc68bf01031a4369074300ebb126b6
clay04 <clayaikenmangeberjr@gmail.com>
2026-06-16 19:22:36 +0800

    ai-engine: change method response

--- ai-engine/app/services/llm/lokalllm_service.py
+++ ai-engine/app/services/llm/lokalllm_service.py
@@ -6,12 +6,10 @@ local_llm_master = ChatOllama(
     base_url=LOCAL_LLM_BASE_URL, 
     model="qwen2.5-coder:3b",
     temperature=0,
-    format="json"
 )
 
 local_llm_fallback = ChatOllama(
     base_url=LOCAL_LLM_BASE_URL, 
     model="qwen2.5-coder:1.5b",
     temperature=0,
-    format="json"
 )
