ea1fb13f436a8648af723ab0207f483c22db962e
clay04 <clayaikenmangeberjr@gmail.com>
2026-06-16 20:05:11 +0800

    ai-engine: stream False

--- ai-engine/app/services/llm/lokalllm_service.py
+++ ai-engine/app/services/llm/lokalllm_service.py
@@ -6,12 +6,14 @@ local_llm_master = ChatOllama(
     base_url=LOCAL_LLM_BASE_URL, 
     model="qwen2.5-coder:1.5b",
     temperature=0,
-    format="json"
+    format="json",
+    stream=False,
 )
 
 local_llm_fallback = ChatOllama(
     base_url=LOCAL_LLM_BASE_URL, 
     model="qwen2.5-coder:3b",
     temperature=0,
-    format="json"
+    format="json",
+    stream=False
 )
